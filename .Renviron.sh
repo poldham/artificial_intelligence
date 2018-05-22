5dbfddd6a162217caaf55d6eda836bbaaf27b67a
Paul Oldham <poldham@mac.com>
2018-05-22 09:30:05 +0100

    update sentiment notes

---
+++ .Renviron.sh
@@ -0,0 +1,16 @@
+TWITTER_PAT=/Users/colinbarnes/twitter_token.rds
+
+cat(paste0("TWITTER_PAT=", file_name),
+    file = file.path(home_directory, ".Renviron"),
+    append = TRUE)
+
+GOOGLE_MAPS_KEY="AIzaSyApqXvTNrKFReMcM_1T59Xv-VNRuiSOIOg"
+
+GL_AUTH="/Users/pauloldham17inch/Desktop/open_source_master/googlenlp/translate-4cd3f27c93e3.json"
+
+getOption(GBIF_USER = "poldham", GBIF_PWD = "Gracetess1", GBIF_EMAIL = "poldham@mac.com")
+
+TWITTER_AI_APP="ai_wipo_analytics"
+TWITTER_AI_CONSUMER_KEY="cVs9kEDEC7vYOFtnSHcrVAETo"
+TWITTER_AI_CONSUMER_SECRET="mvrkKu58CAbYhqtlZvocSKg5WTX5QeiXxbAI8asglVUsraBCQ0"
+TWITTER_PAT=/Users/colinbarnes/.rtweet_token.rds
